74da17adcb092d3b7b38e78603311a5bfca4f42a
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-09-03 20:20:32 +0200

    Add friendship model validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -12,9 +12,33 @@
 #  updated_at   :datetime         not null
 #
 class Friendship < ApplicationRecord
+  validate :no_self_referential_friend_requests
+  validate :request_in_one_direction_only
+  validate :no_requests_to_current_friends, if: -> { requestor }
+
   belongs_to :requestor, class_name: 'User', foreign_key: 'requestor_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
 
   scope :friends, -> { where('status =?', true) }
   scope :not_friends, -> { where('status =?', false) }
+
+  private
+
+  def no_self_referential_friend_requests
+    if requestor_id == receiver_id
+      errors.add(:receiver_id, :blank, message: 'No self referential friend requests allowed.')
+    end
+  end
+
+  def request_in_one_direction_only
+    if Friendship.exists?(requestor_id: receiver_id, receiver_id: requestor_id)
+      errors.add(:receiver_id, :blank, message: 'There is a pending request from this user.')
+    end
+  end
+
+  def no_requests_to_current_friends
+    if requestor.friends.any? { |friend| friend.id == receiver.id }
+      errors.add(:receiver_id, :blank, message: 'User is already friends with this receiver.')
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_03_175154) do
+ActiveRecord::Schema[7.0].define(version: 20_220_903_175_154) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "friendships", force: :cascade do |t|
-    t.bigint "requestor_id", null: false
-    t.bigint "receiver_id", null: false
-    t.boolean "status", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
-    t.index ["requestor_id"], name: "index_friendships_on_requestor_id"
+  create_table 'friendships', force: :cascade do |t|
+    t.bigint 'requestor_id', null: false
+    t.bigint 'receiver_id', null: false
+    t.boolean 'status', default: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['receiver_id'], name: 'index_friendships_on_receiver_id'
+    t.index ['requestor_id'], name: 'index_friendships_on_requestor_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'username', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "friendships", "users", column: "receiver_id"
-  add_foreign_key "friendships", "users", column: "requestor_id"
+  add_foreign_key 'friendships', 'users', column: 'receiver_id'
+  add_foreign_key 'friendships', 'users', column: 'requestor_id'
 end
