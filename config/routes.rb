1d95167039fe74e55b15521c32b962f5ff92251b
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-09-07 19:40:07 +0200

    generate home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  root 'home#indexbash'
+  resources :users
+  
+  root to: 'home#index'
 end
