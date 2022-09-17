7beed8ce84b2448ec37c0786aa0379c3c11d7df2
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-09-17 16:34:17 +0200

    Add seed data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+(1..10).each do |id|
+  User.create!(
+# each user is assigned an id from 1-20
+      id: id, 
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+# issue each user the same password
+      password: "123456", 
+      password_confirmation: "123456",
+  )
+end
