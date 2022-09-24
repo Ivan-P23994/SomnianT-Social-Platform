1c1b8ef902c2580baebc720bbbafed8f447f1c19
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-09-25 01:31:06 +0200

    Add Profiles to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@ sql_query = "SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(i
 
 (1..30).each do |id|
   User.create!(
-      id: id, 
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      email: Faker::Internet.email,
-      password: "123456", 
-      password_confirmation: "123456",
+    id: id, 
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456'
+  )
+
+  Profile.create!(
+    user_id: id,
+    birth_year: DateTime.now - 20 + id.year,
+    birth_month: DateTime.now.month,
+    birth_day: DateTime.now.day,
+    gender: Faker::Gender.binary_type,
+    occupation: Faker::Job.title,
+    birth_place: Faker::Address.city
   )
 end
 
