740de9567764e47251d76c88cb0ac8132a979a4a
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-10-09 13:04:11 +0200

    Add Comments to Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@ def assign_avatar!(profile, name)
     profile.image.attach(io: io, filename: filename)
   end
 end
-
+@users = []
 @profiles = []
 
 ActiveRecord::Base.transaction do
   ActiveRecord::Base.connection.reset_pk_sequence!('users')
 
   (1..30).each do |id|
-    User.create!(
+    @users << User.create!(
       id: id,
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
@@ -35,6 +35,41 @@ ActiveRecord::Base.transaction do
       birth_place: Faker::Address.city
     )
   end
+
+  posts = []
+  180.times do |i|
+    date = Faker::Date.between(from: 2.years.ago, to: Date.today)
+    body = case i
+           when 0..36 then Faker::GreekPhilosophers.quote
+           when 37..72 then Faker::Quotes::Shakespeare.hamlet_quote
+           when 73..108 then Faker::Quotes::Shakespeare.as_you_like_it_quote
+           when 109..144 then Faker::Quotes::Shakespeare.king_richard_iii_quote
+           else Faker::Quotes::Shakespeare.romeo_and_juliet_quote
+           end
+
+    posts << Post.create!(
+      title: Faker::Book.title,
+      body: body,
+      author: @users[rand(@users.length)],
+      created_at: date,
+      updated_at: date
+    )
+  end
+
+   # create comments
+   comments = []
+   posts.each do |post|
+     rand(5).times do
+       user = @users[rand(@users.length)]
+       date = Faker::Date.between(from: post.created_at, to: Date.today)
+       body = Faker::Quote.jack_handey
+       comments << Comment.create!(post: post,
+                                   author: user,
+                                   body: body,
+                                   created_at: date,
+                                   updated_at: date)
+     end
+   end
 
 end
 
