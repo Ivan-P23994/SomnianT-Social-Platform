ec9207242804b4e48c5f540df6639cc4cacb7b3f
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-10-15 01:49:13 +0200

    fix duplicate index migration

--- db/migrate/20220911231456_create_friendships.rb
+++ db/migrate/20220911231456_create_friendships.rb
@@ -5,6 +5,5 @@ class CreateFriendships < ActiveRecord::Migration[7.0]
       t.integer :friend_id
       t.timestamps null: false
     end
-    add_index :friendships, [:user_id, :friend_id], unique: true
   end
 end
