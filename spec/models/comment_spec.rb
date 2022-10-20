f963d5a947d9c146f3bf7b0635e34b062737a8a7
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-10-20 09:12:50 +0200

    Add comment  model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,4 +16,5 @@ RSpec.describe Comment, type: :model do
 
   it { is_expected.to belong_to(:author) }
   it { is_expected.to belong_to(:post) }
+  it { is_expected.to validate_presence_of(:body) }
 end
